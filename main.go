70d76ace0c41d8e78e33ed184073ccfe6783c13e
stormtomten <80994025+stormtomten@users.noreply.github.com>
2026-06-05 00:28:33 +0200

    C.2 L.1 Crawilng

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Fprint(os.Stderr, "no website provided\n")
+		os.Exit(1)
+	} else if len(args) > 1 {
+		fmt.Fprint(os.Stderr, "too many arguments provided\n")
+		os.Exit(1)
+	}
+
+	fmt.Printf("starting crawl of: %s\n", args[0])
 }
